e1bc403ad09a429764bfc72b235a81c59acc8b10
payelle15 <payelle15@gmail.com>
2018-11-13 20:01:30 -0500

    Finalisation de la doc

--- src/update.php
+++ src/update.php
@@ -20,8 +20,8 @@ function validProtocol($dataSource) {
 /**
  * Fonction permettant d'ajouter les erreurs dans la session courante et de
  * faire la redirection vers la page edit.php avec un attribut erreur à true.
- * @param $dataSource : 
- * @param $errorSource
+ * @param $dataSource : les données entrées par l'utilisateur dans le formulaire d'update de l'ouvrage.
+ * @param $errorSource : un tableau contenant les erreurs s'il y a lieu.
  */
 function errorProtocol($dataSource, $errorSource) {
     $id = $_GET['id'];
@@ -30,6 +30,11 @@ function errorProtocol($dataSource, $errorSource) {
     header("Location: edit.php?id=$id&error=true");
 }
 
+/**
+ * Fonction permettant d'éditer l'ouvrage dans la bd.
+ * @param $dataSource : l'information utilisées pour modifier l'ouvrage.
+ * @return mixed : retourne l'id de l'ouvrage.
+ */
 function editBookDB($dataSource) {
     $ouvrage['anneeParution'] = numberFormatting('anneeParution', $dataSource);
     $ouvrage['auteurs'] = textFormatting('auteurs', $dataSource);
@@ -44,6 +49,11 @@ function editBookDB($dataSource) {
     return $ouvrage['id'];
 }
 
+/**
+ * Fonction permettant d'updater l'ouvrage dans la base de données ou
+ * de renvoyer les erreurs du formulaire dans la session courante pour que
+ * l'utilisateur puisse les corrigés.
+ */
 function update() {
     $dataSource = $_POST['donnees'];
     $errorTab = validateForm($dataSource);
--- src/utils.php
+++ src/utils.php
@@ -1,14 +1,35 @@
 <?php
 namespace Utils;
 
+/**
+ * Fonction permettant de retourner l'attribut à afficher ou une chaîne vide s'il
+ * n'y a rien à afficher.
+ * @param $attribute : L'attribut à afficher.
+ * @param $dataSource : La source de données d'où provient l'attribut.
+ * @return string : retourne l'attribut, sinon une chaîne vide.
+ */
 function showValue($attribute, $dataSource) {
     return $dataSource[$attribute] ?? '';
 }
 
+/**Fonction permettant d'afficher les erreurs dans le formulaire.
+ * @param $attribute : l'attribut que l'on veut savoir s'il est en erreur.
+ * @param $dataSource : la source de données où est l'information sur l'attribut.
+ * @param $errorMessages : Le tableau de messages d'erreur.
+ * @return string : retourne le message d'erreur dans le formulaire s'il y a erreur,
+ * sinon retourne une chaîne vide.
+ */
 function showError($attribute, $dataSource, $errorMessages) {
     return in_array($attribute, $dataSource) ? "<span class='error'>$errorMessages[$attribute]</span><br><br>" : '';
 }
 
+/**
+ * Fonction permettant d'afficher les checkbox pour les supports.
+ * @param $key : la clé du tableau de supports.
+ * @param $value : la valeur relié à la clé du tableau de supports.
+ * @param $dataSource : la source de données on se retrouve l'information
+ * @return string : retourne les checkbox cochés s'il y a lieu.
+ */
 function showCheckBox($key, $value, $dataSource) {
     $supportExists = false;
     if (isset($dataSource['supports']) && !empty($dataSource['supports'])) {
@@ -22,21 +43,43 @@ function showCheckBox($key, $value, $dataSource) {
     return "<input id='supports' type='checkbox' name='donnees[supports][$value]' $checked>$value<br>";
 }
 
+/**
+ * Fonction permettant de formatter toutes données numériques.
+ * @param $attribute : l'attribut que l'on veut formatter.
+ * @param $dataSource : la source de données d'où provient l'attribut.
+ * @return mixed|null : retourne l'attribut formatté, sinon null.
+ */
 function numberFormatting($attribute, $dataSource) {
     $data = sanitizeSting($attribute, $dataSource);
     return $data ?? null;
 }
 
+/**
+ * Fonction permettant de formatter du texte.
+ * @param $attribute : l'attribut que l'on veut formatter.
+ * @param $dataSource : la source de données d'où provient l'attribut.
+ * @return string : retourne l'attribut formatté, sinon une chaîne vide.
+ */
 function textFormatting($attribute, $dataSource) {
     $data = sanitizeSting($attribute, $dataSource);
     return ucwords(strtolower($data)) ?? '';
 }
 
+/**
+ * Fonction permettant de formatter le nom des checkbox.
+ * @param $dataSource : la source d'où provient les supports.
+ * @return array : retourne un tableau contenant les supports formattés.
+ */
 function checkBoxFormatting($dataSource) {
     $keys = array_keys($dataSource);
     return array_map('strtolower', $keys);
 }
 
+/**
+ * Fonction permettant d'allé chercher tous les attributs de l'ouvrage.
+ * @param $ouvrage : l'ouvrage d'où on veut allé chercher l'information.
+ * @return mixed : retourne un tableau avec les attributs.
+ */
 function getAttributes($ouvrage) {
     $datas['id'] = getAttribute('id', $ouvrage);
     $datas['titre'] = getAttribute('titre', $ouvrage);
@@ -46,10 +89,21 @@ function getAttributes($ouvrage) {
     return $datas;
 }
 
+/**
+ * Fonction permettant d'allé chercher un attribut spécifique.
+ * @param $attribute : l'attribut que l'on veut.
+ * @param $ouvrage : l'ouvrage d'où est pris l'information sur l'attribut.
+ * @return string : retoune l'attribut, sinon une chaîne vide.
+ */
 function getAttribute($attribute, $ouvrage) {
     return $ouvrage[$attribute] ?? '';
 }
 
+/**
+ * Fonction permettant d'allé chercher les supports.
+ * @param $tab : le tableau contenant les données sur les supports sélectionnés par l'utilisateur.
+ * @return array : retourne un tableau contenant les supports, sinon un tableau vide.
+ */
 function getSupports($tab) {
     $tabSupports = ['kindle' => 'Kindle', 'epub' => 'EPUB', 'papier' => 'Papier', 'pdf' => 'PDF'];
     foreach ($tab as $key => $value) {
@@ -58,6 +112,12 @@ function getSupports($tab) {
     return $support ?? [];
 }
 
+/**
+ * Fonction permettant d'afficher les supports.
+ * @param $supports : le tableau contenant les supports.
+ * @return string : retourne une string formatté pour afficher les supports
+ * dans la bibliographie (index.html).
+ */
 function showSupports($supports) {
     $string = '';
     foreach ($supports as $key => $value) {
@@ -66,6 +126,12 @@ function showSupports($supports) {
     return substr($string, 0, strlen($string) - 2);
 }
 
+/**
+ * Fonction permettant de nettoyer les strings.
+ * @param $attribute : l'attribut à nettoyer.
+ * @param $dataSource : la source de données d'où provient l'attribut.
+ * @return mixed : retourne la string nettoyée.
+ */
 function sanitizeSting($attribute, $dataSource) {
     return filter_var($dataSource[$attribute], FILTER_SANITIZE_STRING);
 }
@@ -108,7 +174,7 @@ function validAttr($attribute, $value) {
  */
 function validateForm($datas) {
     foreach ($datas as $attribute => $value) {
-        if(!validAttr($attribute, $value))
+        if (!validAttr($attribute, $value))
             $errorTab[] = $attribute;
     }
     return $errorTab ?? [];
